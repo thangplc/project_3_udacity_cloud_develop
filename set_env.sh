a93229a06732f3df6c2671dc54036d542e589406
thangplc <thangplc1412@gmail.com>
2024-11-29 22:05:46 +0700

    update

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=monolight-database.c2ovh8zu4ebx.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.c2iil2vrxtal.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=monolight-bucket
+export AWS_BUCKET=elasticbeanstalk-us-east-1-863121142881 
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
