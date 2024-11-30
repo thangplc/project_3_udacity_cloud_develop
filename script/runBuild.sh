589796635b6d7ca1074ff8a6b2407cd9171633de
thangplc <thangplc1412@gmail.com>
2024-11-30 13:44:25 +0700

    update

--- script/runBuild.sh
+++ script/runBuild.sh
@@ -5,3 +5,20 @@ kubectl autoscale deployment backend-user --cpu-percent=70 --min=3 --max=5
 kubectl autoscale deployment backend-feed --cpu-percent=70 --min=3 --max=5
 kubectl autoscale deployment frontend --cpu-percent=70 --min=3 --max=5
 kubectl autoscale deployment reverseproxy --cpu-percent=70 --min=3 --max=5
+
+
+kubectl delete deployment udagram-api-user
+kubectl delete services udagram-api-user
+kubectl delete deployment udagram-api-feed
+kubectl delete services udagram-api-feed
+kubectl delete services udagram-reverseproxy
+kubectl delete services publicreverseproxy
+
+
+kubectl apply -f secrets
+kubectl apply -f deployments/deployment-api-feed.yaml
+kubectl apply -f services/service-api-feed.yaml
+kubectl apply -f deployments/deployment-api-user.yaml
+kubectl apply -f services/service-api-user.yaml
+kubectl apply -f deployments/deployment-reverseproxy.yaml
+kubectl apply -f services/service-reverseproxy.yaml
--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  // apiHost: 'http://localhost:8080/api/v0'
-  apiHost: 'http://ae38c93b158204a0b91188acad757d9f-768992721.us-east-1.elb.amazonaws.com/api/v0'
+  appName: 'Udagram ThangPLC',
+  apiHost: 'http://localhost:8080/api/v0'
+  // apiHost: 'http://ae38c93b158204a0b91188acad757d9f-768992721.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
